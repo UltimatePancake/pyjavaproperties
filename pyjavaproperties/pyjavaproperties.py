3a2c914c65cb7c921861b10963094bd6f5518515
Pier-Angelo Gaetani <pier.gaetani@gmail.com>
2021-07-20 16:32:05 +0200

    Update pyjavaproperties.py

--- pyjavaproperties/pyjavaproperties.py
+++ pyjavaproperties/pyjavaproperties.py
@@ -278,7 +278,7 @@ class Properties(object):
         for key,value in self._props.items():
             out.write(''.join((key,'=',value,'\n')))
 
-    def store(self, out, header="", timestamp=False):
+    def store(self, out, header='', timestamp=False, timeformat='%a %b %d %H:%M:%S %Y'):
         """ Write the properties list to the stream 'out' along
         with the optional 'header' """
 
@@ -286,11 +286,11 @@ class Properties(object):
             raise ValueError('Steam should be opened in write mode!')
 
         try:
-            if header != "":
+            if header != '':
                 out.write(''.join(('# ',header,'\n')))
             if timestamp:
                 # Write timestamp
-                tstamp = time.strftime('%a %b %d %H:%M:%S %Z %Y', time.localtime())
+                tstamp = time.strftime(timeformat, time.localtime())
                 out.write(''.join(('# ',tstamp,'\n')))
             # Write properties from the pristine dictionary
             for prop in self._keyorder:
